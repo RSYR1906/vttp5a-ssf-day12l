9f21ddfdff2fd214c7473eff13f82c6a0aa3ce7f
RSYR1906 <rsim9977@gmail.com>
2024-11-15 21:23:54 +0800

    Updates

--- src/main/java/sg/edu/nus/iss/vttp5a_ssf_day12l/controller/WeatherController.java
+++ src/main/java/sg/edu/nus/iss/vttp5a_ssf_day12l/controller/WeatherController.java
@@ -1,18 +1,30 @@
 package sg.edu.nus.iss.vttp5a_ssf_day12l.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import sg.edu.nus.iss.vttp5a_ssf_day12l.model.Country;
+import sg.edu.nus.iss.vttp5a_ssf_day12l.service.CountryService;
 
 @Controller
 @RequestMapping("/weather")
 public class WeatherController {
 
+    @Autowired
+    CountryService countryService;
+
     @GetMapping("")
-    public String weather(@RequestParam(required = true, name = "country", defaultValue = "Singapore") String city,
+    public String weather(@RequestParam(required = true, name = "country") String city,
             @RequestParam(name = "metrics", defaultValue = "centimeters") String metric, Model model) {
 
         model.addAttribute("city", city);
@@ -21,7 +33,7 @@ public class WeatherController {
         return "weather";
     }
 
-    @GetMapping("/city/{country}/metrics/{metrics}")
+    @GetMapping(path = { "/city/{country}/metrics/{metrics}", "/{country}/{metrics}" })
     public String weather2(@PathVariable(required = true, name = "country") String city,
             @PathVariable(name = "metrics") String metric, Model model) {
 
@@ -30,4 +42,33 @@ public class WeatherController {
 
         return "weather";
     }
+
+    @GetMapping("/pagea")
+    public String pageA(Model model) {
+        String[] units = { "millimiter", "centimeter", "meter", "kilometer" };
+        List<Country> countries = countryService.getCountries();
+
+        model.addAttribute("countries", countries);
+        model.addAttribute("metrics", units);
+        return "pagea";
+    }
+
+    @GetMapping("/forma")
+    public String showForm() {
+        return "forma";
+    }
+
+    @PostMapping("/forma")
+    public String handlePageA(@RequestBody MultiValueMap<String, String> form) {
+
+        // Map<String, String> formData = new HashMap<>();
+
+        for (String str : form.keySet()) {
+            // formData.put(str, form.getFirst(str));
+            System.out.println(str + ">>> " + form.getFirst(str));
+        }
+
+        return "forma";
+    }
+
 }
